7cc4f3e3d0a56ac88a32814e932ab98f87e120a5
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2020-03-27 20:32:12 +0900

    Analytics導入の修正

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueAnalytics from 'vue-analytics'
 // FontAwesome
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 Vue.component('font-awesome-icon', FontAwesomeIcon)
@@ -11,3 +12,9 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+
+Vue.use(VueAnalytics, {
+  id: 'UA-162053243-1',
+  router
+})
