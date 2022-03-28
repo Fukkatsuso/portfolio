a5d7f3650a940c6683fa9d447a64a5dc4f1c3a95
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2022-03-28 23:54:32 +0900

    uninstall: vue/compat

--- vue.config.js
+++ vue.config.js
@@ -2,22 +2,5 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/portfolio/'
     : '/',
-  outputDir: 'docs',
-  chainWebpack: config => {
-    config.resolve.alias.set('vue', '@vue/compat')
-
-    config.module
-      .rule('vue')
-      .use('vue-loader')
-      .tap(options => {
-        return {
-          ...options,
-          compilerOptions: {
-            compatConfig: {
-              MODE: 2
-            }
-          }
-        }
-      })
-  }
+  outputDir: 'docs'
 }
