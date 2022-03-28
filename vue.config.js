511ae28f1277c0f27c2e540504265e7c34e21c6c
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2022-03-28 15:51:57 +0900

    update: vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,22 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/portfolio/'
     : '/',
-  outputDir: 'docs'
+  outputDir: 'docs',
+  chainWebpack: config => {
+    config.resolve.alias.set('vue', '@vue/compat')
+
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap(options => {
+        return {
+          ...options,
+          compilerOptions: {
+            compatConfig: {
+              MODE: 2
+            }
+          }
+        }
+      })
+  }
 }
